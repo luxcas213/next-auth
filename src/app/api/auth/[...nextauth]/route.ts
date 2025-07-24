613611af9bc416843fd5a34369d93c24568cee04
Tu Nombre <lgarbate@gmail.com>
2025-07-24 16:08:59 -0300

    first commit

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,25 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/db";
+import GoogleProvider from "next-auth/providers/google";
+
+const authHandler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: "database",
+  },
+  callbacks: {
+    async session({ session, user }: { session: any; user: any }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
+});
+
+export { authHandler as GET, authHandler as POST };
